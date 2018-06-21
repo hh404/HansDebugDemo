e9b31361681c90bd53d91579e34af984a7d042a1
chinaSoft <chinasoft@chinasofts-macbook-pro.local>
2018-06-21 20:47:34 +0800

    Initial Commit

---
+++ HansDebugDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HansDebugDemo
+//
+//  Created by chinaSoft on 2018/6/21.
+//  Copyright © 2018 chinaSoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
